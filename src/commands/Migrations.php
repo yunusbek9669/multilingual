4d704597fc71de7eb938a287772db41bfa6cfbee
yunusbek <yunusbek.inomov@gmail.com>
2025-01-03 23:58:07 +0500

    red

--- src/commands/Migrations.php
+++ src/commands/Migrations.php
@@ -32,12 +32,12 @@ class {$migrationClassName} extends Migration
     {
         \$this->createTable('{{%language_list}}', [
             'id' => \$this->primaryKey(),
-            'name' => \$this->string(30),
-            'short_name' => \$this->string(5),
-            'key' => \$this->string(5),
+            'name' => \$this->string(30)->notNull(),
+            'short_name' => \$this->string(5)->notNull(),
+            'key' => \$this->string(5)->notNull(),
             'image' => \$this->string(50),
             'import_excel' => \$this->string(50),
-            'table' => \$this->string(50),
+            'table' => \$this->string(50)->notNull(),
             'order_number' => \$this->integer(),
             'status' => \$this->integer(2),
             'created_at' => \$this->bigInteger(),
@@ -49,6 +49,23 @@ class {$migrationClassName} extends Migration
         \$this->createIndex('{{%idx-language_list-status}}', '{{%language_list}}', 'status');
         \$this->createIndex('{{%idx-language_list-created_by}}', '{{%language_list}}', 'created_by');
         \$this->createIndex('{{%idx-language_list-updated_by}}', '{{%language_list}}', 'updated_by');
+        
+        \$this->upsert('language_list', [
+            'id' => 1,
+            'name' => null, # Type the full name of the current language.
+            'short_name' => null, # Type a short name for the current language.
+            'key' => \Yii::$app->language ?? null, # Enter the short code representing the current language. Based on the international standard ISO 639-1 language codes. For example: uz, en, ru.
+            'image' => null, # Enter the path to the flag image for the current language.
+            'import_excel' => '', # Should be empty for now
+            'table' => 'lang_'.\Yii::$app->language, # Enter the name of the table that stores translations for the current language, for example: lang_uz, lang_en, lang_ru. (rule: do not deviate from the standard lang_* pattern)
+            'order_number' => 1,
+            'status' => 1,
+            'created_by' => 1,
+            'created_at' => time(),
+            'updated_by' => 1,
+            'updated_at' => time()
+        ]);
+        \$this->execute("SELECT setval('language_list_id_seq', max(id)) FROM language_list");
     }
 
     public function safeDown()
--- src/models/LanguageManager.php
+++ src/models/LanguageManager.php
@@ -6,37 +6,17 @@ use yii\helpers\ArrayHelper;
 
 class LanguageManager
 {
-    public static function getDefaultLanguage(): array
-    {
-        return [
-            'en' => [
-                'name' => 'Inglish',
-                'short_name' => 'Ing',
-                'image' => '/asset/images/flags/en.jpg',
-                'table' => null,
-            ],
-        ];
-    }
-
-    public static function getModelLanguages(): array|\yii\db\ActiveRecord|null
+    public static function getAllLanguages(string $key): array|\yii\db\ActiveRecord|null
     {
         $Multilingual = BaseLanguageList::find()->where(['status' => 1])->asArray()->all();
-        return ArrayHelper::map($Multilingual, 'key', function ($model) {
+        return ArrayHelper::map($Multilingual, 'key', function ($model) use ($key) {
             return [
                 'name' => $model['name'],
                 'short_name' => $model['short_name'],
                 'image' => $model['image'],
                 'table' => $model['table'],
+                'active' => $key === $model['key'],
             ];
         });
-    }
-
-    public static function getAllLanguages($key = null): array|\yii\db\ActiveRecord|null
-    {
-        $languageList = array_merge(self::getDefaultLanguage(), self::getModelLanguages());
-        if (!is_null($key)){
-            return $languageList[$key];
-        }
-        return $languageList;
     }
 }
