7a572c9600f47b10d34e0255413e4bf06217e1fe
yunusbek <yunusbek.inomov@gmail.com>
2025-03-10 13:59:08 +0500

    set

---
+++ src/Bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+namespace Yunusbek\Multilingual;
+
+use yii\base\BootstrapInterface;
+
+class Bootstrap implements BootstrapInterface
+{
+    public function bootstrap($app)
+    {
+        if ($app instanceof \yii\console\Application) {
+            $app->controllerMap = array_merge($app->controllerMap, [
+                'ml-extract-i18n' => [
+                    'class' => \Yunusbek\Multilingual\commands\Messages::class,
+                    'configFile' => '@vendor/yunusbek/multilingual/src/config/i18n.php',
+                ],
+                'ml-extract-attributes' => [
+                    'class' => \Yunusbek\Multilingual\commands\Messages::class,
+                    'configFile' => '@vendor/yunusbek/multilingual/src/config/attributes.php',
+                ],
+            ]);
+        }
+    }
+}
